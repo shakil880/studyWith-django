f8283f0a61efe4b2a879794a2f69b405e27f8eec
Shakil Quraishi <shakil880@gmail.com>
2022-10-13 14:54:02 +0600

    first commit

---
+++ base/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+
+from base.models import Room, Message, Topic, User
+
+
+admin.site.register(Room)
+admin.site.register(Message)
+admin.site.register(Topic)
+admin.site.register(User)
